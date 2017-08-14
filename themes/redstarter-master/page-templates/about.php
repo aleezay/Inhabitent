53a05e858c716db0124cd4ccc3680d4cb26bffd6
aleezay <aleezayh@hotmail.com>
2017-08-14 15:34:31 -0700

    debugged about.php

--- themes/redstarter-master/page-templates/about.php
+++ themes/redstarter-master/page-templates/about.php
@@ -3,14 +3,14 @@
 * Template Name: About Page
 *@package RED_Starter_Theme
 **/
-get_header(); 
+get_header(); ?>
 
-  <div id="primary class="content-area">
+  <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
       <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
         <header class="entry-header custom-hero">
           <div class="container">
-              <?php the_title( '<h1 class="entry-title">', </h1>);?>
+              <?php the_title( '<h1 class="entry-title">', '</h1>');?>
           </div>
         </header>
 
@@ -19,7 +19,7 @@ get_header();
             <h2>Our Story</h2>
             <?php echo CFS()->get( 'about_our_story' ); ?>
         
-            <h2>Our Team></h2>
+            <h2>Our Team</h2>
             <?php echo CFS()->get( 'about_our_team' ); ?>
           </div>
         </div>
