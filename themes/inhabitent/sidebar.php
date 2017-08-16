051d0753867bf7be8c5bcd7df1b46fc58b4a17cc
aleezay <aleezayh@hotmail.com>
2017-08-16 16:58:03 -0700

    fixed gitignore, working on nav bar

---
+++ themes/inhabitent/sidebar.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The sidebar containing the main widget area.s
+ *
+ * @package Inhabitent_Theme
+ */
+
+if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+	return;
+}
+?>
+
+<div id="secondary" class="widget-area" role="complementary">
+	<!--<div class="sidebar-container">-->
+	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+	<!--</div>-->
+</div><!-- #secondary -->
