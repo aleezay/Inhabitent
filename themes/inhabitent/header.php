b1a02910f01f2df526b9c5b19945b81deee1a08e
aleezay <aleezayh@hotmail.com>
2017-08-16 22:18:48 -0700

    almost finished styling nav bar, need to add search form

--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -21,25 +21,48 @@
 			<a class="skip-link screen-reader-text" href="#content"><?php esc_html( 'Skip to content' ); ?></a>
 
 			<header id="masthead" class="site-header" role="banner">
-			<div class="nav-container">
+			<div class="top-menu-container">
+
 				<div class="site-branding">
-					<h1 class="site-title screen-reader-text">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-					</h1>
-					<div class="logo-container">
-						<img src=<?php echo get_stylesheet_directory_uri();?>/images/logos/inhabitent-logo-tent.svg alt="Inhabitent logo"/></a>
-					</div>
-					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
-				</div><!--nav-container-->
+					<h1 class="site-title screen-reader-text"></h1>			
+										
 				</div><!-- .site-branding -->
 
 				<nav id="site-navigation" class="main-navigation" role="navigation">
+						<div class="nav-logo-container">
+						<img src=<?php echo get_stylesheet_directory_uri();?>/images/logos/inhabitent-logo-tent.svg alt="Inhabitent logo"/></a>
+					</div>
+					<div class="nav-items-container">
 					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
 					<?php esc_html( 'Primary Menu' ); ?></button>
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+					</div>
 				</nav><!-- #site-navigation -->
+
+		<!--ADD SEARCH FORM!!!-->
+
+			<!---<i class="fa fa-search"></i>-->
+
+				</div><!--front-menu-container-->
 			</header><!-- #masthead -->
 
 			<div id="content" class="site-content">
+
+<!--
+header container
+	nav bar container
+		logo container
+			logo
+		nav items container
+			shop container
+				a
+			journal container
+				a
+			about container
+				a
+			find us container
+				a
+		searchbar container
+			search-->
 
 			
